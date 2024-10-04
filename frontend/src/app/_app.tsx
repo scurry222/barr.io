e6f13ecaf022302d47a3fe03bda30474db5654db
scurry <scurry222@gmail.com>
2024-10-03 18:52:16 -0700

    Add graphql queries thru apollo client

---
+++ frontend/src/app/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client';
+import client from "@/lib/apollo-client";
+ 
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+     <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
+}
--- frontend/src/app/messages/page.tsx
+++ frontend/src/app/messages/page.tsx
@@ -1,7 +1,31 @@
 import TextBox from "@/components/Textbox";
+import { useQuery } from "@apollo/client";
+import { GET_MESSAGES } from "@/lib/graphql";
+
+interface Message {
+  id: string
+  text: string
+  timestamp: string
+}
+
+interface GetMessagesData {
+  messages: Message[]
+}
 
 export default function Chat() {
+  const { loading, error, data } = useQuery<GetMessagesData>(GET_MESSAGES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
-    <TextBox />
-  )
+    <div>
+      <h1>Messages</h1>
+      <ul>
+        {data?.messages.map((message) => (
+          <li key={message.id}>{message.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
